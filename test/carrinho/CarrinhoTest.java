2ac4c0496707a36249fa4efcb4174264583093e0
Lucca Gomes <lucca.gomes@gabriel.com.br>
2025-08-25 17:15:03 -0300

    chore: tira o import do asserts Throws não usado

--- test/carrinho/CarrinhoTest.java
+++ test/carrinho/CarrinhoTest.java
@@ -1,7 +1,6 @@
 package carrinho;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
