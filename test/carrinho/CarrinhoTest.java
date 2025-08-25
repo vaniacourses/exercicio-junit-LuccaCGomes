078a31d453dd14a46a7adc764c232f31a0c5fc70
Lucca Gomes <lucca.gomes@gabriel.com.br>
2025-08-25 17:03:52 -0300

    feat: cria testes para verificar se carrinho começa vazio e se item está sendo adicionado corretamente

--- test/carrinho/CarrinhoTest.java
+++ test/carrinho/CarrinhoTest.java
@@ -1,10 +1,42 @@
 package carrinho;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import produto.Produto;
+
 
 @DisplayName("Classe para teste do Carrinho")
 public class CarrinhoTest {
 
-	
+	private Carrinho carrinho;
+
+	@BeforeEach
+	public void inicializa() {
+		carrinho = new Carrinho();
+	}
+
+	@DisplayName("Carrinho inicia vazio e com total 0.0")
+	@Test
+	public void testCarrinhoInicial() {
+		assertEquals(0, carrinho.getQtdeItems());
+		assertEquals(0.0, carrinho.getValorTotal(), 0.0001);
+	}
+
+	@DisplayName("Adicionar item aumenta quantidade e valor total")
+	@Test
+	public void testAddItem() {
+		Produto p1 = new Produto("Mouse", 50.0);
+		Produto p2 = new Produto("Teclado", 150.0);
+
+		carrinho.addItem(p1);
+		carrinho.addItem(p2);
+
+		assertEquals(2, carrinho.getQtdeItems());
+		assertEquals(200.0, carrinho.getValorTotal(), 0.0001);
+	}
 	
 }
